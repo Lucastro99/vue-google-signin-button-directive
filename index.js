14bac5734aadadcfbd51dbdf827706d7fd72b7a0
Lucas de Castro <lucas.rcastro1@gmail.com>
2021-04-27 11:26:50 -0300

    ajuste para o scope

--- index.js
+++ index.js
@@ -1,43 +1,48 @@
-
 import Vue from 'vue'
 
 export default Vue.directive('google-signin-button', {
-  bind: function (el, binding, vnode) {
-    CheckComponentMethods()
-    let clientId = binding.value
-    let googleSignInAPI = document.createElement('script')
-    googleSignInAPI.setAttribute('src', 'https://apis.google.com/js/api:client.js')
-    document.head.appendChild(googleSignInAPI)
+    bind: function(el, binding, vnode) {
+        CheckComponentMethods()
+        console.log(binding);
+        let clientId = binding.value.clientId
+        let Scope = binding.value.scope
+        let googleSignInAPI = document.createElement('script')
+        googleSignInAPI.setAttribute('src', 'https://apis.google.com/js/api:client.js')
+        document.head.appendChild(googleSignInAPI)
 
-    googleSignInAPI.onload = InitGoogleButton
+        googleSignInAPI.onload = InitGoogleButton
 
-    function InitGoogleButton() {
-      gapi.load('auth2', () => {
-        const auth2 = gapi.auth2.init({
-          client_id: clientId,
-          cookiepolicy: 'single_host_origin'
-        })
-        auth2.attachClickHandler(el, {},
-          OnSuccess,
-          Onfail
-        )
-      })
-    }
-    function OnSuccess(googleUser) {
-      vnode.context.OnGoogleAuthSuccess(googleUser.getAuthResponse().id_token)
-      googleUser.disconnect()
-    }
-    function Onfail(error) {
-      vnode.context.OnGoogleAuthFail(error)
-    }
-    function CheckComponentMethods() {
-      if (!vnode.context.OnGoogleAuthSuccess) {
-        throw new Error('The method OnGoogleAuthSuccess must be defined on the component')
-      }
+        function InitGoogleButton() {
+            gapi.load('auth2', () => {
+                const auth2 = gapi.auth2.init({
+                    client_id: clientId,
+                    scope: Scope,
+                    cookiepolicy: 'single_host_origin'
+                })
+                auth2.attachClickHandler(el, {},
+                    OnSuccess,
+                    Onfail
+                )
+            })
+        }
 
-      if (!vnode.context.OnGoogleAuthFail) {
-        throw new Error('The method OnGoogleAuthFail must be defined on the component')
-      }
+        function OnSuccess(googleUser) {
+            vnode.context.OnGoogleAuthSuccess(googleUser.getAuthResponse().id_token)
+            googleUser.disconnect()
+        }
+
+        function Onfail(error) {
+            vnode.context.OnGoogleAuthFail(error)
+        }
+
+        function CheckComponentMethods() {
+            if (!vnode.context.OnGoogleAuthSuccess) {
+                throw new Error('The method OnGoogleAuthSuccess must be defined on the component')
+            }
+
+            if (!vnode.context.OnGoogleAuthFail) {
+                throw new Error('The method OnGoogleAuthFail must be defined on the component')
+            }
+        }
     }
-  }
 })
